2b65e9349809c1ae82fa7b444c69b20bae3aac94
nikseljkee <nikseljkee1@gmail.com>
2020-11-08 21:19:24 +0300

    Add entity models

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext
+    {
+        public SolarDbContext() { }
+
+        public SolarDbContext(DbContextOptions<SolarDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInventory> ProductInventories { get; set; }
+        public DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+        public DbSet<SalesOrder> SalesOrders { get; set; }
+        public DbSet<SalesOrderItem> SalesOrderItems { get; set; }
+    }
+}
--- SolarCoffee.Web/Startup.cs
+++ SolarCoffee.Web/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SolarCoffee.Data;
 
 namespace SolarCoffee.Web
 {
@@ -22,13 +24,16 @@ namespace SolarCoffee.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SolarDbContext>(options =>
+            {
+                options.EnableDetailedErrors();
+                options.UseNpgsql(Configuration.GetConnectionString("solar.dev"));
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
